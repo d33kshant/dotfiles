42adfd79b9326fabc156ff56071ee0141335abd4
Deekshant Yadav <d33kshant@gmail.com>
2022-08-19 22:27:00 +0530

    Added main setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Install neccessary packages
+sudo dnf install -y git light rofi feh
+
+# Install packages for customizing the desktop
+dnf install -y lxappearance materia-gtk-theme papirus-icon-theme jetbrains-mono-fonts
+
+# Setup Brave Browser
+sudo dnf install dnf-plugins-core
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser
+
+# Setup VS Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf install code

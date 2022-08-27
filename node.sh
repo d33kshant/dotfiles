2dcecc451876e33cf9e3ce092fb878e2c6f11ca8
Deekshant Yadav <d33kshant@gmail.com>
2022-08-27 23:23:53 +0530

    Created install script for node and yarn

---
+++ node.sh
@@ -0,0 +1,8 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# `node` is alias for latest version
+nvm install node
+nvm use node
+
+# install yarn globally
+sudo npm i -g yarn

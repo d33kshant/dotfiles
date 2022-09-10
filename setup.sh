595185203192a02bc88266f91978f01d4b539125
Deekshant Yadav <contactdeekshant+git@gmail.com>
2022-09-10 18:57:15 +0530

    Added dependancies for screenshot script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install neccessary packages
-sudo dnf install -y git light rofi feh xclip imagemagick
+sudo dnf install -y git light rofi feh gvfs gvfs-mtp tumbler ffmpegthumbnailer xclip imagemagick
 
 # Install packages for customizing the desktop
 dnf install -y lxappearance materia-gtk-theme papirus-icon-theme jetbrains-mono-fonts
